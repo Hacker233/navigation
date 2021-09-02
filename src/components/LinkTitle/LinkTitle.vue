23a0e7c2a5ae753892bf3aae5b02d6a1c145999c
hacker233 <lhqhacker@163.com>
2021-09-02 20:06:53 +0800

    添加标题

---
+++ src/components/LinkTitle/LinkTitle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="link-title-box">
+        <div class="link-title">
+            <h1>我是标题</h1>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.link-title-box {
+    height: 50px;
+    margin: 0 0 20px 0;
+    box-sizing: border-box;
+    min-width: 100px;
+    border-bottom: 5px solid rgb(211, 159, 81);
+    display: flex;
+    justify-content: flex-start;
+    .link-title {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        h1 {
+            font-size: 20px;
+        }
+    }
+}
+</style>
--- src/views/movie/index.vue
+++ src/views/movie/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="movie-box">
     <!-- 网站推荐盒子 -->
-    <div class="movie-list-mox">
+    <div class="movie-list-mox" v-for="(item, index) of 9" :key="index">
+      <!-- 链接标题 -->
+      <div class="link-title-content">
+        <link-title></link-title>
+      </div>
       <!-- 链接列表 -->
       <div class="movie-card">
         <Link-card v-for="(item, index) of 17" :key="index"></Link-card>
@@ -10,10 +14,12 @@
   </div>
 </template>
 <script>
-import LinkCard from "@/components/LinkCard/LinkCard.vue";
+import LinkCard from "@/components/LinkCard/LinkCard.vue"; // 链接卡片
+import LinkTitle from "@/components/LinkTitle/LinkTitle.vue"; // 标题
 export default {
   components: {
     LinkCard,
+    LinkTitle,
   },
 };
 </script>
@@ -22,16 +28,19 @@ export default {
   flex: 1;
   .movie-list-mox {
     width: 100%;
-    padding: 20px 0;
+    padding: 20px 20px;
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
+    .link-title-content {
+      display: flex;
+    }
     .movie-card {
       display: grid;
       grid-template-columns: repeat(auto-fit, minmax(257px, 1fr));
       grid-row-gap: 40px;
       grid-column-gap: 25px;
-      place-items: center;
+      place-items: flex-start;
     }
   }
 }
