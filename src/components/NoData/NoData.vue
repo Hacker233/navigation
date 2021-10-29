10f24b1c35fd797618756fbfac51840629e3d050
hacker233 <lhqhacker@163.com>
2021-10-29 11:07:47 +0800

    添加分类

---
+++ src/components/NoData/NoData.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="no-data-box">
+    <i class="iconfont pig-meiyougengduo"></i>
+    <p>暂无数据</p>
+  </div>
+</template>
+<style lang="scss" scoped>
+.no-data-box {
+  height: 100%;
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .iconfont {
+    font-size: 80px;
+    margin-bottom: 20px;
+    color: green;
+  }
+  p {
+    font-size: 16px;
+  }
+}
+</style>
--- src/views/articleList/index.vue
+++ src/views/articleList/index.vue
@@ -1,36 +1,60 @@
 <template>
   <div class="soft-ware-box">
     <!-- 左侧列表区域 -->
-    <div class="left-list">
-      <!-- 文章筛选 -->
-      <div class="screen-box">
+    <div class="left-box">
+      <!-- 分类筛选 -->
+      <div class="category-box">
         <ul>
           <li
-            v-for="(item, index) in sortList"
-            :key="index"
-            @click="selectSort(item.sort)"
-            :class="[{ active: sort === item.sort }]"
+            :class="[{ 'category-active': selectCategoryIndex === 0 }]"
+            @click="selectAll"
           >
-            <i :class="['iconfont', item.iconfontClass]"></i>
-            <span>{{ item.label }}</span>
+            全部
+          </li>
+          <li
+            v-for="(item, index) in categoryList"
+            :key="index"
+            :class="[{ 'category-active': selectCategoryIndex === index + 1 }]"
+            @click="selectCategory(item, index)"
+          >
+            {{ item.category_name }}
           </li>
         </ul>
       </div>
-      <!-- 文章列表 -->
-      <el-skeleton :rows="4" animated :loading="!articleList.length" />
-      <template>
-        <list-card
-          v-for="(item, index) in articleList"
-          :key="index"
-          :articleInfo="item"
-        ></list-card>
-        <!-- 分页 -->
-        <Pagination
-          :total="total"
-          :pageCount="pageCount"
-          @currentChange="currentChange"
-        ></Pagination>
-      </template>
+      <div class="left-list">
+        <!-- 文章筛选 -->
+        <div class="screen-box">
+          <ul>
+            <li
+              v-for="(item, index) in sortList"
+              :key="index"
+              @click="selectSort(item.sort)"
+              :class="[{ active: sort === item.sort }]"
+            >
+              <i :class="['iconfont', item.iconfontClass]"></i>
+              <span>{{ item.label }}</span>
+            </li>
+          </ul>
+        </div>
+        <!-- 加载骨架 -->
+        <el-skeleton :rows="4" animated :loading="isShowSkeleton" />
+        <!-- 无数据 -->
+        <no-data v-if="isShowNoData"></no-data>
+        <!-- 文章列表 -->
+        <template>
+          <list-card
+            v-for="(item, index) in articleList"
+            :key="index"
+            :articleInfo="item"
+          ></list-card>
+          <!-- 分页 -->
+          <Pagination
+            :total="total"
+            :pageCount="pageCount"
+            @currentChange="currentChange"
+          ></Pagination>
+        </template>
+      </div>
     </div>
     <!-- 右侧推荐区域 -->
     <div class="right-record">
@@ -42,12 +66,17 @@
 <script>
 import ListCard from "./components/ListCard.vue";
 import { getArticleList } from "@/http/api/article";
+import { getCategory } from "@/http/api/category";
 import Pagination from "@/components/Pagination/Pagination"; // 分页组件
 import HotList from "./components/HotList"; // 热榜
+import NoData from "@/components/NoData/NoData"; // 无数据
 export default {
   data() {
     return {
-      articleList: [],
+      articleList: [], // 文章列表
+      categoryList: [], // 分类列表
+      isShowSkeleton: true, // 是否显示骨架屏
+      isShowNoData: false, // 是否展示无数据页面
       pageParams: {
         page: 1,
         pageSize: 10,
@@ -55,6 +84,8 @@ export default {
       total: 0, // 总条数
       pageCount: 0, // 总页数
       sort: "new", // 排序规则：最新-hot、热门-hot、点赞最多-like
+      category: "", // 选择的分类
+      selectCategoryIndex: 0,
       sortList: [
         {
           sort: "new",
@@ -74,17 +105,46 @@ export default {
       ],
     };
   },
-  components: { ListCard, Pagination, HotList },
+  components: { ListCard, Pagination, HotList, NoData },
   mounted() {
-    this.getArticleListAsync();
+    this.getArticleListAsync(); // 获取文章列表
+    this.getCategoryAsync(); // 获取分类列表
   },
   methods: {
+    // 获取分类列表
+    async getCategoryAsync() {
+      const data = await getCategory();
+      if (data.code === "00000") {
+        this.categoryList = data.data.data;
+      } else {
+        this.$message({
+          message: data.message,
+          type: "error",
+        });
+      }
+    },
+    // 选择全部
+    selectAll() {
+      this.selectCategoryIndex = 0;
+      this.category = "";
+      this.sort = "new";
+      this.getArticleListAsync(); // 更新文章列表
+    },
+    // 选择分类
+    selectCategory(item, index) {
+      this.selectCategoryIndex = index + 1;
+      this.category = item.category_name;
+      this.sort = "new";
+      this.getArticleListAsync(); // 更新文章列表
+    },
     // 根据菜单id获取文章列表
     async getArticleListAsync() {
+      this.isShowSkeleton = true;
       this.articleList = [];
       let params = {
         menuId: this.$route.query.menuId,
         sort: this.sort,
+        category: this.category,
         page: this.pageParams.page,
         pageSize: this.pageParams.pageSize,
       };
@@ -93,11 +153,19 @@ export default {
         this.articleList = data.data.data;
         this.total = data.data.page.count; // 总条数
         this.pageCount = data.data.page.pageCount; // 总页数
+        this.isShowSkeleton = false;
+        if (this.articleList.length) {
+          this.isShowNoData = false;
+        } else {
+          this.isShowNoData = true;
+        }
       } else {
         this.$message({
           message: data.message,
           type: "error",
         });
+        this.isShowSkeleton = false;
+        this.isShowNoData = true;
       }
     },
     // 筛选
@@ -119,44 +187,77 @@ export default {
   display: flex;
   align-items: flex-start;
   justify-content: center;
-  .left-list {
-    width: 750px;
+  .left-box {
     margin-right: 20px;
-    background-color: #fff;
-    padding: 10px 20px;
-    .screen-box {
+    .category-box {
       width: 100%;
-      height: 40px;
-      /* border-bottom: 1px solid #ccc; */
+      height: 45px;
+      background-color: #fff;
       margin-bottom: 10px;
+      display: flex;
+      align-items: center;
+      padding: 0 0 0 20px;
+      box-sizing: border-box;
       position: sticky;
       top: 50px;
-      background: #fff;
       z-index: 1;
+      border-bottom: 1px solid #eee;
       ul {
         display: flex;
-        align-items: center;
-        list-style: none;
-        padding: 0;
         margin: 0;
-        height: 100%;
-        .active {
-          color: green;
-        }
+        padding: 0;
         li {
+          list-style: none;
+          margin-right: 15px;
+          font-size: 15px;
           cursor: pointer;
-          font-size: 14px;
-          display: flex;
-          align-items: center;
           &:hover {
             color: green;
           }
-          &:nth-child(2) {
-            margin: 0 20px;
+        }
+        .category-active {
+          color: green;
+        }
+      }
+    }
+    .left-list {
+      width: 750px;
+      background-color: #fff;
+      padding: 10px 20px;
+      .screen-box {
+        width: 100%;
+        height: 40px;
+        /* border-bottom: 1px solid #ccc; */
+        margin-bottom: 10px;
+        position: sticky;
+        top: 95px;
+        background: #fff;
+        z-index: 1;
+        ul {
+          display: flex;
+          align-items: center;
+          list-style: none;
+          padding: 0;
+          margin: 0;
+          height: 100%;
+          .active {
+            color: green;
           }
-          .iconfont {
-            font-size: 12px;
-            margin-right: 5px;
+          li {
+            cursor: pointer;
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+            &:hover {
+              color: green;
+            }
+            &:nth-child(2) {
+              margin: 0 20px;
+            }
+            .iconfont {
+              font-size: 12px;
+              margin-right: 5px;
+            }
           }
         }
       }
