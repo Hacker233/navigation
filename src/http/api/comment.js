c894e2db016605fbf1e33d1188102a97fd167759
hacker233 <lhqhacker@163.com>
2021-11-22 20:01:09 +0800

    bug

--- src/http/api/comment.js
+++ src/http/api/comment.js
@@ -1,9 +1,9 @@
 import axiosService from "../index";
 // 获取评论列表
 export const getCommentList = (params) => {
-  return axiosService.get("/comment/commentList", { params });
+  return axiosService.get("/api/comment/commentList", { params });
 };
 // 发表评论
 export const addComment = (data) => {
-  return axiosService.post("/comment/addComment", data);
+  return axiosService.post("/api/comment/addComment", data);
 };
