f44a9cf523e708e9753841e6a3161219e9a42ac7
hacker233 <lhqhacker@163.com>
2021-11-25 19:49:30 +0800

    修改分类

--- src/views/content/category/index.vue
+++ src/views/content/category/index.vue
@@ -15,6 +15,12 @@
     >
       <el-table-column prop="category_id" label="分类ID" sortable>
       </el-table-column>
+      <el-table-column prop="category_menu_id" label="所属菜单id" sortable>
+      </el-table-column>
+      <el-table-column prop="category_menu_parent_id" label="所属菜单父级id" sortable>
+      </el-table-column>
+      <el-table-column prop="category_menu_name" label="所属菜单名" sortable>
+      </el-table-column>
       <el-table-column prop="category_name" label="分类名称"> </el-table-column>
       <!-- 操作列 -->
       <el-table-column label="操作" width="150">
@@ -43,6 +49,14 @@
       width="30%"
     >
       <el-form ref="ruleForm" :rules="rules" :model="form" label-width="80px">
+        <el-form-item label="所属菜单" prop="menuInfo">
+          <el-cascader
+            v-model="form.menuInfo"
+            :options="menuListOptions"
+            :show-all-levels="false"
+            @change="changeOwnMenu"
+          ></el-cascader>
+        </el-form-item>
         <el-form-item label="分类名称" prop="categoryName">
           <el-input
             v-model="form.categoryName"
@@ -88,13 +102,44 @@ export default {
       pageCount: 0, // 总页数
       form: {
         categoryName: "",
+        menuInfo: [], // 所属菜单相关信息
       },
       rules: {
+        menuInfo: [
+          { required: true, message: "请选择所属菜单", trigger: "blur" },
+        ],
         categoryName: [
           { required: true, message: "请填写分类名称", trigger: "blur" },
         ],
       },
     };
+  },
+  computed: {
+    // 初始化菜单筛选下拉菜单
+    menuListOptions() {
+      let options = [];
+      this.$store.state.topmenuList.forEach((item) => {
+        if (!item.topmenu_child.length) {
+          options.push({
+            value: JSON.stringify(item),
+            label: item.topmenu_name,
+          });
+        } else {
+          let children = item.topmenu_child.map((item) => {
+            return {
+              value: JSON.stringify(item),
+              label: item.topmenu_name,
+            };
+          });
+          options.push({
+            value: JSON.stringify(item),
+            label: item.topmenu_name,
+            children: children,
+          });
+        }
+      });
+      return options;
+    },
   },
   components: {
     Pagination,
@@ -121,6 +166,11 @@ export default {
         });
       }
     },
+    /*********所属菜单**********/
+    changeOwnMenu(value) {
+      console.log(value);
+    },
+    
     addCategory() {
       this.modelTitle = "新增";
       this.categoryDialogVisible = true;
@@ -149,11 +199,21 @@ export default {
       this.pageParams.page = currentPage; // 更改查询页面
       this.init();
     },
+    // 所属菜单——筛选选中得值
+    filterMenu() {
+      return this.form.menuInfo.length > 1
+        ? this.form.menuInfo[1]
+        : this.form.menuInfo[0];
+    },
     // 新增分类
     async addCategoryAsync() {
+      let menuInfo = JSON.parse(this.filterMenu());
       this.btnIsLoadgin = true;
       let parasm = {
         categoryName: this.form.categoryName,
+        categoryMenuName: menuInfo.topmenu_name, // 文章所属菜单名称
+        categoryMenuParentId: menuInfo.parent_topmenu_id, // 文章所属于菜单的父级id
+        categoryMenuId: menuInfo.topmenu_id, // 文章所属菜单id
       };
       const data = await addCategory(parasm);
       if (data.code === "00000") {
