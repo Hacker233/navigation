5e12ffb5109438fffc4e21e8f544c36b3eba027e
hacker233 <lhqhacker@163.com>
2021-12-04 17:07:10 +0800

    bug

--- src/common/auth.js
+++ src/common/auth.js
@@ -2,6 +2,9 @@ import router from "@/router/index";
 import login from "../common/LoginDialog"; // 登录弹窗
 // 路由前置守卫
 router.beforeEach((to, from, next) => {
+  if (to.path) {
+    window._hmt.push(['_trackPageview', to.fullPath]);
+  }
   let token = localStorage.getItem("token");
   if (token || !to.meta.auth) {
     next();
